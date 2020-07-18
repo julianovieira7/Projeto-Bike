7a28f1883c68dd143a024c1c3f0b013931b21ee0
victor <victor@victor-550XBE-350XBE>
2020-07-18 14:14:00 -0300

    criando crud tipoProduto

---
+++ ProjectAdventureBike/src/controller/TipoProdutoController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import model.TipoProduto;
+import repository.TipoProdutoRepository;
+
+@Named
+@ViewScoped
+public class TipoProdutoController extends Controller<TipoProduto>{
+	private String filtro;
+	private List<TipoProduto> listaTipoProduto;
+
+	public void pesquisar() {
+//		EntityManager em = JPAFactory.getEntityManager();
+//		Query query = em.createQuery("Select a " + "From TipoProduto a " + "Where upper(a.nome) like upper(:filtro)");
+//		query.setParameter("filtro", "%" + getFiltro() + "%");
+//		listaTipoProduto = query.getResultList();
+
+		TipoProdutoRepository repo = new TipoProdutoRepository();
+		listaTipoProduto = repo.findByNome(getFiltro());
+	}
+
+
+	@Override
+	public TipoProduto getEntity() {
+		if (entity == null)
+			entity = new TipoProduto();
+		return entity;
+	}
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+
+	public List<TipoProduto> getListaTipoProduto() {
+		if (listaTipoProduto == null)
+			listaTipoProduto = new ArrayList<TipoProduto>();
+
+		return listaTipoProduto;
+	}
+
+}
---
+++ ProjectAdventureBike/src/model/TipoProduto.java
@@ -0,0 +1,25 @@
+package model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class TipoProduto extends DefaultEntity<TipoProduto> {
+	
+	private static final long serialVersionUID = -2359731594073401155L;
+	private String tipoProduto;
+
+	public String getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(String tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+//	@Override
+//	public Validation<TipoProduto> getValidation() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+}
